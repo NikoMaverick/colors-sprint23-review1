42dab641257b7caa7604124b1d855cf6c0d21858
Niko Maverick <maverick.nk@gmail.com>
2024-11-17 21:51:43 +0100

    Cambian las letras de color pero no el fondo

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,7 +1,30 @@
-function boxColor ({color, value}) {
+import { useState, useRef, useEffect, } from "react";
+
+function BoxColor ({color, value}) {
+  const [classBox, setClassBox] = useState('')
+  const boxRef = useRef(null)
+
+  const boxClass = {
+    backgroundColor: classBox == `box ${value}` ? color : null,
+    border: `2px solid ${color}`,
+    color: color
+  }
+
+  useEffect(() => {
+    setClassBox(boxRef.current.classBox)
+  },[])
+
   return (
     <>
+    <div 
+    ref={boxRef}
+    className={`box ${color}`}
+    style={boxClass}
+    >
+    <h1>{color === value ? `Soy color ${color}` : `No soy color ${value}`} </h1>
+      
+    </div>
     </> 
   )
 }
-export default boxColor;  
+export default BoxColor;  
--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,9 +1,28 @@
+import { useEffect, useState, useRef } from "react";
+import BoxColor from "./BoxColor";
 
+const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
 
 function MyForm() {
+  //const [value, setValue] = useState('')
+  const [inputColor, setInputColor] = useState('')
+  const inputRef = useRef(null); // Esta variable la podemos llamar como queramos
 
   return (
-    <></>
+    <>
+    <input 
+    type="text"
+    placeholder="Ingresa el color"
+    value={inputColor}
+    ref={inputRef}
+    onChange={() => setInputColor(inputRef.current.value)}
+    />
+     <div className="container">
+      {colors.map((color, i) => (
+        <BoxColor key={i} color={color} value={inputColor}/>
+      ))}
+    </div>
+    </>
   );
 }
 
